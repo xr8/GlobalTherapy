b5c1e362e94797cff886f8cf8744c0bf8402055d
BIOHIZARD <biohizard@gmail.com>
2023-01-26 16:56:26 -0600

    update img rand menu

--- web/application/controllers/Inicio.php
+++ web/application/controllers/Inicio.php
@@ -18,8 +18,10 @@ class Inicio extends CI_Controller
         $data['css']             = 'user';
         $data['js']              = 'libs,GlobalTherapy,utility,main';
         $rand = rand(1,3);
+        $rand2 = rand(1,2);
         $data['logo']            = CDN_URL."img/inicio/logo".$rand.".svg";
-
+        $data['bgImg']           = CDN_URL."img/inicio/bgimg-".$rand2.".jpg";
+        
             $this->load->view('loop/header', $data);
             $this->load->view('loop/top'   , $data);
             $this->load->view('loop/menuoffcanvas', $data);
--- web/application/views/loop/menuoffcanvas.php
+++ web/application/views/loop/menuoffcanvas.php
@@ -32,7 +32,7 @@
 <!--Search Popup End Here-->
 
 <!--Side Nav Start Here-->
-<div class="sidenav" style="background-image: url(<?php echo CDN_URL+$bgImg;?>);background-size: cover;">
+<div class="sidenav" style="background-image: url(<?php echo $bgImg;?>);background-size: cover;">
     <div class="sidenav-wrap">
         <div class="logo-area">
             <div class="logo">
